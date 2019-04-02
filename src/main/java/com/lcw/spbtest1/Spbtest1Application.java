bb880893eb9d1d9b0a3cda73dfec5763f37b6806
liuchengwei <2781127305@qq.com>
2019-04-02 21:49:04 +0800

    第一次使用

---
+++ src/main/java/com/lcw/spbtest1/Spbtest1Application.java
@@ -0,0 +1,13 @@
+package com.lcw.spbtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spbtest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spbtest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcw/spbtest1/Spbtest1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lcw.spbtest1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spbtest1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
